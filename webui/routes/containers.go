d878d1f7cba76f1eb1ed37b78e65d8d18db7ca78
Lennart Espe <lnsp@users.noreply.github.com>
2019-06-26 01:24:04 +0200

    Add support for listing containers

--- webui/routes/containers.go
+++ webui/routes/containers.go
@@ -1,8 +1,73 @@
 package routes
 
-import "net/http"
+import (
+	"errors"
+	"net/http"
+
+	"github.com/docker/docker/api/types/swarm"
+	humanize "github.com/dustin/go-humanize"
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+type ContainerInstance struct {
+	ID              string
+	Service         string
+	Image, ImageTag string
+	Node            string
+	Created         string
+}
+
+type ContainerContext struct {
+	BaseContext
+	Containers []ContainerInstance
+}
 
 func (router *Router) showContainers(w http.ResponseWriter, r *http.Request) {
-	context := router.getBaseContext().SetActive("Containers")
+	context, err := router.getContainerContext()
+	if err != nil {
+		router.showError(w, r, err)
+		return
+	}
 	router.templates["containers.html"].Execute(w, context)
+}
+
+func (router *Router) getServiceByID(id string) (*swarm.Service, error) {
+	for _, ep := range router.endpoints {
+		svc, err := ep.InspectService(id)
+		if err != nil {
+			continue
+		}
+		return svc, nil
+	}
+	return nil, errors.New("failed to contact instance")
+}
+
+func (router *Router) getContainerContext() (ContainerContext, error) {
+	ctx := ContainerContext{
+		BaseContext: router.getBaseContext(),
+		Containers:  []ContainerInstance{},
+	}
+	for _, ep := range router.endpoints {
+		tasks, err := ep.ListTasks(docker.ListTasksOptions{})
+		if err != nil {
+			continue
+		}
+		for _, t := range tasks {
+			svc, err := router.getServiceByID(t.ServiceID)
+			if err != nil {
+				continue
+			}
+			base, tag := router.getBaseImageFromTagDigest(t.Spec.ContainerSpec.Image)
+			ctx.Containers = append(ctx.Containers, ContainerInstance{
+				ID:       t.ID,
+				Service:  svc.Spec.Name,
+				Image:    base,
+				ImageTag: tag,
+				Node:     t.NodeID,
+				Created:  humanize.Time(t.CreatedAt),
+			})
+		}
+	}
+	ctx.SetActive("Containers")
+	return ctx, nil
 }
