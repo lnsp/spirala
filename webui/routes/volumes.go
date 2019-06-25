06bc3d8a5118d97288863ea7026924dd9a6aa32a
Lennart Espe <lnsp@users.noreply.github.com>
2019-06-26 01:48:35 +0200

    Add volume listing

--- webui/routes/volumes.go
+++ webui/routes/volumes.go
@@ -1,8 +1,53 @@
 package routes
 
-import "net/http"
+import (
+	"net/http"
+
+	humanize "github.com/dustin/go-humanize"
+	docker "github.com/fsouza/go-dockerclient"
+)
+
+type VolumeInstance struct {
+	Name    string
+	Driver  string
+	Created string
+}
+
+type VolumeContext struct {
+	BaseContext
+	VolumeCount int
+	Volumes     []VolumeInstance
+}
 
 func (router *Router) showVolumes(w http.ResponseWriter, r *http.Request) {
-	context := router.getBaseContext().SetActive("Volumes")
+	context, err := router.getVolumeContext()
+	if err != nil {
+		router.showError(w, r, err)
+		return
+	}
 	router.templates["volumes.html"].Execute(w, context)
+}
+
+func (router *Router) getVolumeContext() (VolumeContext, error) {
+	context := VolumeContext{
+		BaseContext: router.getBaseContext(),
+		Volumes:     []VolumeInstance{},
+	}
+	context.SetActive("Volumes")
+	for _, ep := range router.endpoints {
+		volumes, err := ep.ListVolumes(docker.ListVolumesOptions{})
+		if err != nil {
+			return VolumeContext{}, err
+		}
+		for _, vol := range volumes {
+			context.Volumes = append(context.Volumes, VolumeInstance{
+				Name:    vol.Name,
+				Driver:  vol.Driver,
+				Created: humanize.Time(vol.CreatedAt),
+			})
+			context.VolumeCount++
+		}
+
+	}
+	return context, nil
 }
